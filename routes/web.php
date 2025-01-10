410c05fdfbab13aed4f1ada7eb5fa6abd70956c9
Alberto Zarraga <zarraga555@gmail.com>
2025-01-10 12:03:26 -0400

    Se agregaron rutas para la parte de Ingresos

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Income\Create as IncomeCreate;
+use App\Livewire\Income\Edit as IncomeEdit;
+use App\Livewire\Income\View as IncomeView;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,4 +17,12 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::middleware(['auth:sanctum', 'verified'])->group(function (){
+//        Incomes Routes
+        Route::get('incomes',\App\Livewire\IncomeComponent::class )->name('income.index');
+        Route::get('incomes/create', IncomeCreate::class)->name('income.create');
+        Route::get('incomes/{id}/edit',IncomeEdit::class )->name('income.edit');
+        Route::get('incomes/{id}/view',IncomeView::class )->name('income.view');
+    });
 });
