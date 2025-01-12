d8a5f1b49b030b373012469f43468510431d084a
Alberto Zarraga <zarraga555@gmail.com>
2025-01-12 14:56:44 -0400

    Se agregado los seeders para llenar los datos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
+        $this->call(ItemsEgresos::class);
+        $this->call(ItemsIngresos::class);
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('password'),
         ]);
     }
 }
