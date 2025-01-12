3ac568233c64864aaf2087e5c2d964f602944318
Alberto Zarraga <zarraga555@gmail.com>
2025-01-12 16:16:19 -0400

    Cotrolador principal y Vista principal de Cuentas bancarias

---
+++ app/Livewire/AccountLettersComponent.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\AccountLetters;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class AccountLettersComponent extends Component
+{
+    use WithPagination;
+
+    public string $search = '';
+
+    public function render()
+    {
+        $accountLetters = AccountLetters::where('bank_name', 'like', '%'.$this->search.'%')
+                                            ->orWhere('account_name', 'like', '%'.$this->search.'%')
+                                            ->paginate(10);
+        return view(
+            'livewire.account-letters-component',
+            compact('accountLetters')
+        )->layout('layouts.app');
+    }
+}
