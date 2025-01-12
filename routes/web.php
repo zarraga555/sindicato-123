b148577b5237576132afa01e1f7914f934f31fc8
Alberto Zarraga <zarraga555@gmail.com>
2025-01-12 14:55:02 -0400

    Se agregageron nuevas rutas para Cuentas bancarias

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\Income\Create as IncomeCreate;
 use App\Livewire\Income\Edit as IncomeEdit;
 use App\Livewire\Income\View as IncomeView;
+use App\Livewire\AccountLetters\Create as AccountLettersCreate;
+use App\Livewire\AccountLetters\Edit as AccountLettersEdit;
+use App\Livewire\AccountLetters\View as AccountLettersView;
 
 Route::get('/', function () {
     return view('welcome');
@@ -24,5 +27,10 @@ Route::middleware([
         Route::get('incomes/create', IncomeCreate::class)->name('income.create');
         Route::get('incomes/{id}/edit',IncomeEdit::class )->name('income.edit');
         Route::get('incomes/{id}/view',IncomeView::class )->name('income.view');
+        //        Account Letters Routes
+        Route::get('account-letters',\App\Livewire\AccountLettersComponent::class )->name('accountLetters.index');
+        Route::get('account-letters/create', AccountLettersCreate::class)->name('accountLetters.create');
+        Route::get('account-letters/{id}/edit',AccountLettersEdit::class )->name('accountLetters.edit');
+        Route::get('account-letters/{id}/view',AccountLettersView::class )->name('accountLetters.view');
     });
 });
