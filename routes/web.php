9d7d57f5c94a331a22b0b3dbce49c851d0630058
Alberto Zarraga <zarraga555@gmail.com>
2025-01-13 01:20:25 -0400

    Se agregagaron rutas para las opciones de Ingresos y egresos de sus categorias

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,13 @@ use App\Livewire\Income\View as IncomeView;
 use App\Livewire\AccountLetters\Create as AccountLettersCreate;
 use App\Livewire\AccountLetters\Edit as AccountLettersEdit;
 use App\Livewire\AccountLetters\View as AccountLettersView;
+use App\Livewire\IncomeCategories\Create as IncomeCategoriesCreate;
+use App\Livewire\IncomeCategories\Edit as IncomeCategoriesEdit;
+use App\Livewire\IncomeCategories\View as IncomeCategoriesView;
+use App\Livewire\ExpenseCategories\Create as ExpenseCategoriesCreate;
+use App\Livewire\ExpenseCategories\Edit as ExpenseCategoriesEdit;
+use App\Livewire\ExpenseCategories\View as ExpenseCategoriesView;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -32,5 +39,15 @@ Route::middleware([
         Route::get('account-letters/create', AccountLettersCreate::class)->name('accountLetters.create');
         Route::get('account-letters/{id}/edit',AccountLettersEdit::class )->name('accountLetters.edit');
         Route::get('account-letters/{id}/view',AccountLettersView::class )->name('accountLetters.view');
+        //        Income Categories Routes
+        Route::get('imcome-categories',\App\Livewire\IncomeCategoriesComponent::class )->name('incomeCategories.index');
+        Route::get('imcome-categories/create', IncomeCategoriesCreate::class)->name('incomeCategories.create');
+        Route::get('imcome-categories/{id}/edit',IncomeCategoriesEdit::class )->name('incomeCategories.edit');
+        Route::get('imcome-categories/{id}/view',IncomeCategoriesView::class )->name('incomeCategories.view');
+        //        Expense Categories Routes
+        Route::get('expense-categories',\App\Livewire\ExpenseCategoriesComponent::class )->name('expenseCategories.index');
+        Route::get('expense-categories/create', ExpenseCategoriesCreate::class)->name('expenseCategories.create');
+        Route::get('expense-categories/{id}/edit',ExpenseCategoriesEdit::class )->name('expenseCategories.edit');
+        Route::get('expense-categories/{id}/view',ExpenseCategoriesView::class )->name('expenseCategories.view');
     });
 });
