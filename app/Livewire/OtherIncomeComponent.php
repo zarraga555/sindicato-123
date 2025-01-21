2729d9a5b129c9849b30b7939a758f024019be40
Alberto Zarraga <zarraga555@gmail.com>
2025-01-21 01:59:09 -0400

    Se creo el archivo de otros ingreso para que puedan agregar ingresos referentes no enfocados a su sindicato

---
+++ app/Livewire/OtherIncomeComponent.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\CashFlow;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class OtherIncomeComponent extends Component
+{
+    use WithPagination;
+
+    public $search = '';
+
+    public function render()
+    {
+        $otherIncomes = CashFlow::where('transaction_type_income_expense', 'income')
+            ->where(function ($query) {
+                $query //->where('name', 'like', '%' . $this->search . '%') // Búsqueda en nombre del gasto
+                ->orWhereHas('vehicles', function ($subQuery) { // Relación con Vehicle
+                    $subQuery->where('id', 'like', '%' . $this->search . '%');
+                })
+                    ->orWhereHas('banks', function ($subQuery) { // Relación con AccountBank
+                        $subQuery->where('bank_name', 'like', '%' . $this->search . '%');
+                    })
+                    ->orWhereHas('itemsCashFlow', function ($subQuery) { // Relación con Item
+                        $subQuery->where('name', 'like', '%' . $this->search . '%');
+                    });
+            })
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+        return view('livewire.other-income-component', compact('otherIncomes'))->layout('layouts.app');
+    }
+}
