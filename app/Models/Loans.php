bb2c153044356def6916edfb0fec65b0eb89061d
Alberto Zarraga <zarraga555@gmail.com>
2025-01-25 17:54:54 -0400

    Se ha modificado el modelo con nuevas columnas y nuevas funcines para su funcionamiento

--- app/Models/Loans.php
+++ app/Models/Loans.php
@@ -9,17 +9,36 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Loans extends Model
 {
     use SoftDeletes;
+
     protected $fillable = [
         'vehicle_id',
-        'driver_partner',
-        'dateLoan',
+//        'driver_id',
+//        'partner_id',
+        'driver_partner_name',
+        'loan_start_date',
         'numberInstalments',
         'debtStatus',
-        'amountLoan'
+        'amountLoan',
+        //News columns
+        'interest_rate',
+        'total_debt',
+        'user_type', //TEMPORALMENTE
+        'payment_frequency',
+        'cash_flows_id',
     ];
 
     public function vehicles(): BelongsTo
     {
         return $this->belongsTo(Vehicle::class, 'vehicle_id');
     }
+
+//    public function drivers(): BelongsTo
+//    {
+//        return $this->belongsTo(Driver::class, 'driver_id');
+//    }
+
+//    public function partners(): BelongsTo
+//    {
+//        return $this->belongsTo(Partner::class, 'partner_id');
+//    }
 }
