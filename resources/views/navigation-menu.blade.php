81be58e7bfd527d9aab97dd2af64effac06f7c03
zarraga555 <zarraga555@gmail.com>
2025-01-29 07:21:33 +0000

    modificaciones en el menu

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -192,7 +192,7 @@
                     </li>
                     <!--
                     <li>
-                        <a href="{{ route('vehicle.report') }}"
+                        <a href="#"
                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
                             {{__('Vehicle Report')}}</a>
                     </li>
@@ -209,7 +209,7 @@
                     </li>
                     <!--
                     <li>
-                        <a href="{{ route('loan.report') }}"
+                        <a href="#"
                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
                            {{__('Loan Report')}}</a>
                     </li>
