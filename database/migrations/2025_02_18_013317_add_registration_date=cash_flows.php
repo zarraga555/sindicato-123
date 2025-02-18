bfb4ded6739f5c61465e18a1a54eac8d701c0a2f
Alberto Zarraga <zarraga555@gmail.com>
2025-02-18 02:22:18 -0400

    Nuevas columnas agreagadas a diferentes tablas

---
+++ database/migrations/2025_02_18_013317_add_registration_date=cash_flows.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cash_flows', function (Blueprint $table) {
+            $table->datetime('registration_date')->nullable()->after('vehicle_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cash_flows', function (Blueprint $table) {
+            $table->dropColumn('registration_date')->nullable();
+        });
+    }
+};
