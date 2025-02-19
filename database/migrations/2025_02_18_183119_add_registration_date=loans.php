bb054095fe8905a2bf8d3c2d3955d3b310ca856e
Alberto Zarraga <zarraga555@gmail.com>
2025-02-18 20:11:22 -0400

    nueva columna cagregada

---
+++ database/migrations/2025_02_18_183119_add_registration_date=loans.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('loans', function (Blueprint $table) {
+            $table->datetime('registration_date')->nullable()->after('amountLoan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('loans', function (Blueprint $table) {
+            $table->dropColumn('registration_date')->nullable();
+        });
+    }
+};
