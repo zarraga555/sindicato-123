333e81208afe5ebe754621da19e4343133ccf110
Alberto Zarraga <zarraga555@gmail.com>
2025-02-26 04:03:58 -0400

    Se agrego icon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" href="{{asset('images/icon/icon.ico')}}" type="image/x-icon">
+        <title>@yield('title') | {{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
