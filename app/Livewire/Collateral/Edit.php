3b74dacdf77ee9c2db93962331170a2b49211161
Alberto Zarraga <zarraga555@gmail.com>
2025-03-30 16:50:17 -0400

    feat(collateral): add edit component for collateral management
    Introduce a new Livewire component to handle editing and deleting collateral records. The component includes functionality to update collateral details, delete collateral records, and manage related payment plans. This enhances the user experience by providing a centralized interface for collateral management.

---
+++ app/Livewire/Collateral/Edit.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Livewire\Collateral;
+
+use App\Models\CashFlow;
+use App\Models\Collateral;
+use App\Models\paymentPlans;
+use App\Models\Vehicle;
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Livewire\Component;
+
+class Edit extends Component
+{
+    // Personal Information
+    public int $vehicle_id;
+    public string $user_type;
+    public string $driver_partner_name;
+    public string $description;
+    // Collateral information
+    public $start_date;
+    public string $payment_frequency;
+    public int $instalments;
+    public float $amount;
+    //
+    public $collateral;
+    public $confirmingUserDeletion = false;
+
+    public function mount($id)
+    {
+        $this->collateral = Collateral::findOrFail($id);
+        $this->vehicle_id = $this->collateral->vehicle_id;
+        $this->user_type = $this->collateral->user_type;
+        $this->driver_partner_name = $this->collateral->driver_partner_name;
+        $this->description = $this->collateral->description;
+        $this->start_date = $this->collateral->start_date;
+        $this->payment_frequency = $this->collateral->payment_frequency;
+        $this->instalments = $this->collateral->instalments;
+        $this->amount = $this->collateral->amount;
+    }
+
+    /**
+     * Muestra el modal de confirmación de eliminación.
+     */
+    public function openDelete()
+    {
+        $this->confirmingUserDeletion = true;
+    }
+
+    /**
+     * Cierra el modal de confirmación de eliminación.
+     */
+    public function closeDelete()
+    {
+        $this->confirmingUserDeletion = false;
+    }
+
+    /**
+     * Elimina el registro de la base de datos.
+     */
+    public function delete()
+    {
+        DB::beginTransaction();
+        try {
+            if (!$this->collateral) {
+                throw new \Exception('Collateral not found.');
+            }
+            // Eliminar registros en el orden correcto
+
+            paymentPlans::where('collateral_id', $this->collateral->id)->delete();
+            Collateral::findOrFail($this->collateral->id)->delete();
+
+            $this->closeDelete();
+            DB::commit();
+            session()->flash('message', 'Registro eliminado correctamente.');
+            return redirect()->route('collateral.index');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            session()->flash('error', 'Error al eliminar: ' . $e->getMessage());
+        }
+    }
+
+    public function update()
+    {
+        // Verifica si el vehiculo existe
+        if (Vehicle::find($this->vehicle_id)) {
+            DB::beginTransaction();
+            try {
+                $this->processUpdateLoan();
+                DB::commit();
+                $this->resetForm();
+                session()->flash('success', 'Datos guardados correctamente.');
+                return redirect()->route('collateral.view', ['id' => $this->collateral->id]);
+            } catch (Exception $e) {
+                DB::rollBack();
+                session()->flash('error', 'Error al guardar los datos: ' . $e->getMessage());
+            }
+        } else {
+            session()->flash('error', 'El vehículo seleccionado no existe. Por favor, verifica e intenta nuevamente.');
+        }
+    }
+
+    private function processUpdateLoan()
+    {
+        $collateral = Collateral::find($this->collateral->id);
+        $collateral->update([
+            'vehicle_id' => $this->vehicle_id,
+            'user_type' => $this->user_type,
+            'driver_partner_name' => $this->driver_partner_name,
+            'description' => $this->description,
+        ]);
+    }
+
+    public function render()
+    {
+        return view('livewire.collateral.edit');
+    }
+}
