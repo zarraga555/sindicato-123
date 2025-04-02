389ce83a5281c7e9ddc4d1488fb4fc2f148662d3
Alberto Zarraga <zarraga555@gmail.com>
2025-04-01 22:23:06 -0400

    fix(loan): correct input field ID from total_debt to interest_rate
    The input field ID was incorrectly set to 'total_debt'. This commit changes it to 'interest_rate' to align with its intended purpose and ensure proper functionality.

--- resources/views/livewire/loan/create.blade.php
+++ resources/views/livewire/loan/create.blade.php
@@ -213,7 +213,7 @@
                         class="fi-input-wrp flex rounded-lg shadow-sm ring-1 transition duration-75 bg-white dark:bg-white/5 focus-within:ring-2 ring-gray-950/10 dark:ring-white/20 focus-within:ring-primary-600 dark:focus-within:ring-primary-500 fi-fo-text-input overflow-hidden">
                         <input
                             class="fi-input block w-full border-none py-1.5 text-base text-gray-950 transition duration-75 placeholder:text-gray-400 focus:ring-0 disabled:text-gray-500 dark:text-white dark:placeholder:text-gray-500 sm:text-sm sm:leading-6 bg-transparent ps-3 pe-3"
-                            id="total_debt"
+                            id="interest_rate"
                             type="number"
                             step="0.01"
                             onblur="formatDecimals(this)"
