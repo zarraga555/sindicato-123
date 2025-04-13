894398ae58f3fc76a6e8d411372bea8775c42ae1
Alberto Zarraga <zarraga555@gmail.com>
2025-04-12 21:35:27 -0400

    feat(cash-drawer): add PDF report generation functionality
    Introduce a new method `generateReport` to generate and download a PDF report for cash drawer transactions. This enhances the functionality by providing a printable and downloadable summary of cash drawer activities.

--- app/Livewire/CashDrawers/Show.php
+++ app/Livewire/CashDrawers/Show.php
@@ -6,6 +6,7 @@ use App\Models\CashDrawer;
 use App\Models\CashFlow;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class Show extends Component
 {
@@ -36,13 +37,13 @@ class Show extends Component
     private function cashOnHandParcial()
     {
         $sum = 0;
-    
+
         // Ejecutar la consulta correctamente con get()
         $items = CashFlow::where('cash_drawer_id', $this->cashDrawer->id)
             ->where('payment_type', 'cash')
             ->where('payment_status', 'paid')
             ->get(); // <- Aquí está la clave
-    
+
         // Iterar sobre la colección
         foreach ($items as $item) {
             if ($item->transaction_type_income_expense == 'income') {
@@ -51,18 +52,18 @@ class Show extends Component
                 $sum -= $item->amount;
             }
         }
-        
+
         return $sum;
     }
 
     private function cashtotalRegistration()
     {
         $sum = 0;
-    
+
         // Ejecutar la consulta correctamente con get()
         $items = CashFlow::where('cash_drawer_id', $this->cashDrawer->id)
             ->get(); // <- Aquí está la clave
-    
+
         // Iterar sobre la colección
         foreach ($items as $item) {
             if ($item->transaction_type_income_expense == 'income') {
@@ -71,7 +72,25 @@ class Show extends Component
                 $sum -= $item->amount;
             }
         }
-        
+
         return $sum;
+    }
+
+    public function generateReport()
+    {
+        $cashFlows = CashFlow::where('cash_drawer_id', $this->cashDrawer->id);
+        $cashRegisters = CashFlow::where('cash_drawer_id', $this->cashDrawer->id)->paginate(25);
+        $cashDrawer = $this->cashDrawer;
+        $currency = $this->currency;
+        $totalRegistration = $this->totalRegistration;
+        $cashOnHand = $this->cashOnHand;
+        // Generar el PDF
+        $pdf = PDF::loadView('pdf.cashDrawer-report', compact('cashFlows', 'cashRegisters', 'cashDrawer', 'currency', 'totalRegistration', 'cashOnHand'));
+
+        // Descargar el archivo PDF
+        return response()->streamDownload(
+            fn() => print($pdf->output()),
+            "reporte_de_cierre_de_caja_" . now()->format('d-m-Y') . ".pdf"
+        );
     }
 }
