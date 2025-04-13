ed3df77121914a0648fe353c4191746c9a1d12e3
Alberto Zarraga <zarraga555@gmail.com>
2025-04-12 21:36:35 -0400

    feat: add cash drawer report PDF view
    Introduce a new Blade template for generating PDF reports of cash drawer closings. This template includes details such as user information, cash status, and transaction summaries to provide a comprehensive overview of cash drawer activities.

---
+++ resources/views/pdf/cashDrawer-report.blade.php
@@ -0,0 +1,112 @@
+@extends('layouts.pdf')
+@section('title', 'Reporte de Cierre de Caja')
+@section('content')
+    <div class="title">{{ __('Cash closing') }}</div>
+    <table style="width: 100%; border: none;">
+        <tr>
+            <td style="width: 33%; padding: 5px; border: none;"><strong>{{ __('User') }}:</strong><br>
+                {{ $cashDrawer->user->name }}
+            </td>
+            <td style="width: 33%; padding: 5px; border: none;"><strong>{{ __('Date of open cash') }}:</strong><br>
+                {{ $cashDrawer->start_time }}
+            </td>
+            <td style="width: 33%; padding: 5px; border: none;"><strong>{{ __('Cash closing date') }}:</strong><br>
+                {{ $cashDrawer->end_time ?? __('Unclosed cash register') }}
+            </td>
+        </tr>
+
+        <tr>
+            <td style="padding: 5px; border: none;"><strong>{{ __('Amount recorded by the system') }}:</strong><br>
+                {{ $currency }}. {{ $cashDrawer->final_money }}
+            </td>
+            <td style="padding: 5px; border: none;"><strong>{{ __('Total amount entered into the system') }}:</strong><br>
+                {{ $currency }}. {{ $cashDrawer->total_calculated }}
+            </td>
+            <td style="padding: 5px; border: none;"><strong>{{ __('Total amount declared by the user') }}:</strong><br>
+                {{ $currency }}. {{ $cashDrawer->total_calculated }}
+            </td>
+        </tr>
+
+        <tr>
+            <td style="padding: 5px; border: none;"><strong>{{ __('Difference') }}:</strong><br>
+                {{ $currency }}. {{ $cashDrawer->difference }}
+            </td>
+            <td style="padding: 5px; border: none;"><strong>{{ __('Cash status') }}:</strong><br>
+                {{ __($cashDrawer->status) }}
+            </td>
+            <td style="padding: 5px; border: none;">
+                <strong>{{ __('Banknoting of the amount declared in cash') }}:</strong><br>
+                @php
+                    $denominations = json_decode($cashDrawer->denominations, true);
+                @endphp
+                <ul style="margin: 0; padding-left: 15px;">
+                    @if ($denominations && is_array($denominations))
+                        @foreach ($denominations as $denomination => $quantity)
+                            @if ($quantity)
+                                <li><strong>{{ $currency }}{{ str_replace('_', '', $denomination) }}:</strong>
+                                    {{ $quantity }}</li>
+                            @endif
+                        @endforeach
+                    @endif
+                </ul>
+            </td>
+        </tr>
+    </table>
+
+    <table style="width: 100%; border-collapse: collapse; font-size: 12px; margin-top: 30px;" border="1"
+        cellpadding="5">
+        <thead>
+            <tr style="background-color: #f0f0f0;">
+                <th>{{ __('Date') }}</th>
+                <th>{{ __('Detail') }}</th>
+                <th>{{ __('Type') }}</th>
+                <th>{{ __('Status') }}</th>
+                <th>{{ __('Total Registration') }}</th>
+                <th>{{ __('Cash on Hand') }}</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse($cashRegisters as $cashRegister)
+                <tr>
+                    <td>{{ \Carbon\Carbon::parse($cashRegister->registration_date)->format('d-m-Y (H:i)') }}</td>
+                    <td>
+                        @if ($cashRegister->vehicle_id != null)
+                            Movil: {{ $cashRegister->vehicle_id }} -
+                            {{ $cashRegister->itemsCashFlow->name }}
+                        @else
+                            {{ $cashRegister->itemsCashFlow->name }}
+                        @endif
+                    </td>
+                    <td> {{ __($cashRegister->transaction_type_income_expense) }}</td>
+                    <td>
+                        {{ __($cashRegister->payment_status) }}
+                        @if ($cashRegister->payment_type != null)
+                            <br>
+                            {{ '(' . __($cashRegister->payment_type) . ')' }}
+                        @endif
+                    </td>
+                    <td>
+                        Bs.
+                        {{ $cashRegister->transaction_type_income_expense != 'income' ? '-' : '' }}{{ number_format($cashRegister->amount, 2) }}
+                    </td>
+                    <td>
+                        Bs.
+                        {{ $cashRegister->payment_type == 'cash' ? ($cashRegister->transaction_type_income_expense != 'income' ? '-' : '') . number_format($cashRegister->amount, 2) : '0.00' }}
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="6" style="text-align: center;">
+                        {{ __('No records were found for today. You can create one now to start recording information.') }}
+                    </td>
+                </tr>
+            @endforelse
+
+            <tr style="font-weight: bold; background-color: #f9f9f9;">
+                <td colspan="4" style="text-align: right;">{{ __('Total Revenues') }}</td>
+                <td>Bs. {{ number_format($totalRegistration, 2) }}</td>
+                <td>Bs. {{ number_format($cashOnHand, 2) }}</td>
+            </tr>
+        </tbody>
+    </table>
+@endsection
