23745c30bc86cb4c6117863b137e2603566f1002
Alberto Zarraga <zarraga555@gmail.com>
2025-04-13 18:23:44 -0400

    feat(CashDrawers): add grouped cash flow data to PDF report
    Introduce grouped cash flow data to the PDF report generation for cash drawers. This change includes summarizing cash flows by transaction type and item, enhancing the report's detail and usability. Additionally, a new method `generateSimplePDF` is added for generating a simplified version of the report.

--- app/Livewire/CashDrawers/Show.php
+++ app/Livewire/CashDrawers/Show.php
@@ -6,6 +6,7 @@ use App\Models\CashDrawer;
 use App\Models\CashFlow;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Illuminate\Support\Facades\DB;
 use Barryvdh\DomPDF\Facade\Pdf;
 
 class Show extends Component
@@ -78,14 +79,57 @@ class Show extends Component
 
     public function generateReport()
     {
+        $cashResgistersBoolean = true;
         $cashFlows = CashFlow::where('cash_drawer_id', $this->cashDrawer->id);
         $cashRegisters = CashFlow::where('cash_drawer_id', $this->cashDrawer->id)->paginate(25);
         $cashDrawer = $this->cashDrawer;
         $currency = $this->currency;
         $totalRegistration = $this->totalRegistration;
         $cashOnHand = $this->cashOnHand;
+        $groupedCashFlow = CashFlow::select(
+            'transaction_type_income_expense',
+            'items_id',
+            DB::raw('SUM(amount) as total_amount'),
+            DB::raw('COUNT(*) as total_count')
+        )
+            // ->where('transaction_status', 'closed')
+            ->where('cash_drawer_id', $this->cashDrawer->id)
+            ->groupBy('transaction_type_income_expense', 'items_id')
+            ->with('itemsCashFlow') // Si tienes relación con Items
+            ->get()
+            ->groupBy('transaction_type_income_expense');
         // Generar el PDF
-        $pdf = PDF::loadView('pdf.cashDrawer-report', compact('cashFlows', 'cashRegisters', 'cashDrawer', 'currency', 'totalRegistration', 'cashOnHand'));
+        $pdf = PDF::loadView('pdf.cashDrawer-report', compact('cashFlows', 'cashRegisters', 'cashDrawer', 'currency', 'totalRegistration', 'cashOnHand', 'groupedCashFlow', 'cashResgistersBoolean'));
+
+        // Descargar el archivo PDF
+        return response()->streamDownload(
+            fn() => print($pdf->output()),
+            "reporte_de_cierre_de_caja_" . now()->format('d-m-Y') . ".pdf"
+        );
+    }
+
+    public function generateSimplePDF()
+    {
+        $cashResgistersBoolean = false;
+        $cashFlows = CashFlow::where('cash_drawer_id', $this->cashDrawer->id);
+        $cashDrawer = $this->cashDrawer;
+        $currency = $this->currency;
+        $totalRegistration = $this->totalRegistration;
+        $cashOnHand = $this->cashOnHand;
+        $groupedCashFlow = CashFlow::select(
+            'transaction_type_income_expense',
+            'items_id',
+            DB::raw('SUM(amount) as total_amount'),
+            DB::raw('COUNT(*) as total_count')
+        )
+            // ->where('transaction_status', 'closed')
+            ->where('cash_drawer_id', $this->cashDrawer->id)
+            ->groupBy('transaction_type_income_expense', 'items_id')
+            ->with('itemsCashFlow') // Si tienes relación con Items
+            ->get()
+            ->groupBy('transaction_type_income_expense');
+        // Generar el PDF
+        $pdf = PDF::loadView('pdf.cashDrawer-report', compact('cashFlows', 'cashDrawer', 'currency', 'totalRegistration', 'cashOnHand', 'groupedCashFlow', 'cashResgistersBoolean'));
 
         // Descargar el archivo PDF
         return response()->streamDownload(
