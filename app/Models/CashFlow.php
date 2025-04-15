053d7a7ce6f916475ab7f1576ac3f022215007bf
Alberto Zarraga <zarraga555@gmail.com>
2025-04-15 17:06:43 -0400

    feat: add method to calculate total recorded money by system
    This commit introduces a new method `totalRecordedByTheSystem` in the `CashFlow` model to calculate the total money recorded by the system for a given cash drawer. The method processes open transactions, sums income amounts, and subtracts expense amounts, returning the final balance. This functionality is essential for accurately closing cash drawers and ensuring financial integrity.

--- app/Models/CashFlow.php
+++ app/Models/CashFlow.php
@@ -26,11 +26,11 @@ class CashFlow extends Model
         'payment_status',
         'transaction_status',
         'cash_drawer_id'
-//        'driver_id',
-//        'partner_id'
+        //        'driver_id',
+        //        'partner_id'
     ];
 
-    public function users (): BelongsTo
+    public function users(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id');
     }
@@ -48,5 +48,29 @@ class CashFlow extends Model
     public function vehicles(): BelongsTo
     {
         return $this->belongsTo(Vehicle::class, 'vehicle_id');
+    }
+
+    public static function totalRecordedByTheSystem($cash_drawer_id)
+    {
+        $final_money = 0;
+        // Procesar transacciones abiertas
+        $cash_flows = CashFlow::where('transaction_status', 'open')
+            ->where('cash_drawer_id', $cash_drawer_id)
+            ->get();
+
+        if ($cash_flows->isEmpty()) {
+            throw new \Exception('No hay transacciones abiertas para cerrar.');
+        }
+
+        foreach ($cash_flows as $account) {
+            $amount = $account->amount;
+
+            if ($account->transaction_type_income_expense === 'income') {
+                $final_money += $amount;
+            } else {
+                $final_money -= $amount;
+            }
+        }
+        return $final_money;
     }
 }
